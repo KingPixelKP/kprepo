b61ed74e2e3afc46f4454d53fb8973f0dc77de20
Gabriel-Matias <gabrielmf1977@gmail.com>
2026-05-03 20:05:37 +0100

    Fix

--- packages/a/auto-cli/xmake.lua
+++ packages/a/auto-cli/xmake.lua
@@ -2,7 +2,7 @@ package("auto-cli")
     set_description("The auto-cli package")
 
     add_urls("https://github.com/KingPixelKP/auto-cli.git")
-    add_versions("0.1.0", "fb6e2e4808b14fda5345c333c83ba7e2fa3d167f")
+    add_versions("0.1.0", "b2dd7725eb634a6903743cae7904f1d337d546a4")
 
     on_install(function (package)
         local configs = {}
