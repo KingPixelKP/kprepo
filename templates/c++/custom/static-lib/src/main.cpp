3e3cfb50550877cd7c40bcf93d07ee3f7e9b9def
GabrielMatiasFalcão <kingpixel2005@gmail.com>
2026-05-04 15:51:22 +0100

    fix

--- templates/c++/custom/static-lib/src/main.cpp
+++ templates/c++/custom/static-lib/src/main.cpp
@@ -5,7 +5,7 @@ int main() {
     int b = 3;
 
     int sum = foo::add(a, b);
-    int difference = foo::subtract(a, b);
+    //int difference = foo::subtract(a, b); //is private, so this line will cause a compilation error
 
     return 0;
 }
